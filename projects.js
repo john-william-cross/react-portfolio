6d2eabb222903543feb25e38794c5e26db1f860b
John Cross <john.william.cross@gmail.com>
2020-10-12 10:51:08 -0700

    add jpg images create projects array and objects

---
+++ projects.js
@@ -0,0 +1,29 @@
+const projects = [
+{
+    id: "2bbe3624-be57-47d5-9435-f1495be75e38",
+    title: "Validation of Email and Password on Account Creation",
+    rating: 4,
+    desc: 
+    "Validating password and email fields in Ask a Teacher, following best security and UX practices.",
+    totalMinutes: 3,
+    postedAt: 1602523447997,
+    githubUrl: "https://github.com/john-william-cross/ask-a-teacher",
+    image:"validation-of-email-and-password-on-account-creation.jpg"
+    youtubeUrl: "https://www.youtube.com/watch?v=wLNF4YaMOwk",
+    isActive: true,
+
+},
+{
+    id: "6fcc8682-9f5f-4bd9-82dc-cfc0212eff55",
+    title: "Walkthrough of the Ask a Teacher Prototype",
+    rating: 5,
+    desc: "Walkthrough of Ask a Teacher, an app in which teachers answer questions submitted by users.",
+    totalMinutes: 2,
+    postedAt: 1602523434372,
+    githubUrl: "https://github.com/john-william-cross/ask-a-teacher",
+    image: "walkthrough-of-the-ask-a-teacher-prototype.jpg",
+    youtubeUrl: "https://www.youtube.com/watch?v=3N6EtIoZBRQ&t=5s",
+    isActive: true,
+
+},
+]
